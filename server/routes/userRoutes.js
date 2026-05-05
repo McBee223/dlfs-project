c5d7d37f6f2516d9bdd26fbd6b60928bfbfe07cd
romar <mcbee2893@gmail.com>
2026-05-06 02:51:03 +0800

    Fix controller import

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notifController = require('../controllers/notificationController');
+const notifController = require('../controllers/NotificationController');
 const userController = require('../controllers/userController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
